d2d34aff46865f6c6409ac3a0e080fbcd48a9f92
DavidGarciaZapata3751 <dgarciaz1@eafit.edu.co>
2026-09-14 17:35:29 -0500

    feat: initialize NestJS backend with TypeORM, SQLite, and books module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
+import { HomeModule } from './home/home.module.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BooksModule } from './books/books.module.js';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'database.sqlite',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    HomeModule,
+    BooksModule
+  ],
 })
 export class AppModule {}
+
